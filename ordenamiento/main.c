c3bdbcfae96cf966af41fb20361d68992aaf1a5f
juancc1001 <52534704+juancc1001@users.noreply.github.com>
2019-09-15 21:22:17 -0300

    Add files via upload

--- ordenamiento/main.c
+++ ordenamiento/main.c
@@ -44,9 +44,9 @@ void ordenarVectorInt(int vec[], int tam, int criterio)
 
     int aux;
 
-    for(int i = 0; i < TAM-1; i++)
+    for(int i = 0; i < tam-1; i++)
     {
-        for(int j = i + 1; j < TAM; j++)
+        for(int j = i + 1; j < tam; j++)
         {
                 if( vec[j] < vec[i] && !criterio )
                 {
